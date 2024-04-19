4ca07775028ec4b6112e572225e84b5909e2fdc6
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-04-19 08:25:33 -0500

    Add expect

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # wetherMapAPI
+
 For the test, an APIKey is required that is generated through the URL https://openweathermap.org/api, first you must create an account and then log in, once we have the APIKey, we configure it in our project, in this case to To have control over the information and ensure confidentiality, the cypress.env.json file is created that will contain the APIKey value, this file is added to the gitignore file so that it is not tracked.
 
 In this repository you will find the following tests:
+
 1. Ensure that the API correctly returns the current weather data when queried with a city name.
 2. Confirm that the API can retrieve current weather data using a city's unique ID.
 3. Test the API's ability to return a 5-day weather forecast for a specified city by name.
 
 # Recommendation:
+
 Please note that to successfully run these tests, you must generate your APIKEY and configure it in the project.
+
+# Notes
+
+To verify specific aspects of the daily forecast. For example, count the number of entries to ensure there is 5 days' worth of data. Keep in mind that each day has 8 entries (every 3 hours), so we would expect 40 entries for 5 days.
--- cypress/e2e/weatherTest.cy.js
+++ cypress/e2e/weatherTest.cy.js
@@ -4,6 +4,7 @@ describe('Weather API Tests', () => {
   const weatherApi = new WeatherAPI(apiKey);
   it('Ensure the API returns the current weather data for a city', () => {
     weatherApi.getWeatherByCityName('City of Santiago').then((response) => {
+      console.log(JSON.stringify(response))
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('weather');
       expect(response.body).to.have.property('main');
@@ -11,6 +12,7 @@ describe('Weather API Tests', () => {
   });
   it('Confirm the API retrieves weather data using a city ID', () => {
     weatherApi.getWeatherByCityId('2172814').then((response) => {
+      console.log(JSON.stringify(response))
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('weather');
       expect(response.body).to.have.property('main');
@@ -18,9 +20,11 @@ describe('Weather API Tests', () => {
   });
   it('Test the APIs ability to return a 5-day forecast for a specified city', () => {
     weatherApi.getForecastByCityName('Colombia').then((response) => {
+      console.log(JSON.stringify(response))
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('list');
       expect(response.body.list.length).to.be.greaterThan(0);
+      expect(response.body.list).to.have.length(40);
     })
   })
 })
